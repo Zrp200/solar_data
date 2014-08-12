6e352c4f7fdfa849b5adfc1e2a5c17e7cc536e50
Zachary Perlmutter <zrp200@gmail.com>
2014-08-11 23:20:48 -0700

    Update solar_data.rb

--- lib/solar_data.rb
+++ lib/solar_data.rb
@@ -1,14 +1,16 @@
 class Planet
-	# Creates a new Planet. Parameter mass sets its mass, parameter density sets its density
+	# Creates a new Planet. Parameter mass sets its mass, parameter density sets its density, and parameter gravity sets its gravity
     	def initialize(mass, density=1, gravity=1, orbit=nil)
     		@mass, @density, @gravity, @orbit = mass, density, gravity, orbit
     	end
 end
 class Earth < Planet
 	DENSITY = 1
+	def mass; 1.3 x 1025; end
 	def density; DENSITY; end
 	def gravity; 1; end
 	def initialize; end
 end
+
 		
 		
