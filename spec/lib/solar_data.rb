16fa4dede99cc083df52f8d171a316306a3697c5
Zachary Perlmutter <zrp200@gmail.com>
2014-08-12 12:27:18 -0700

    Update solar_data.rb

--- spec/lib/solar_data.rb
+++ spec/lib/solar_data.rb
@@ -1,1 +1,25 @@
 require 'spec_helper.rb'
+include SolarData
+describe Planet do
+    subject {Earth()}
+    	describe "#mass" do
+    		it "should == 1.3x1025" do
+    			expect(subject.mass).to eq BigDecimal("1.3x1025")
+    		end
+    		it "should be a BigDecimal" do
+    			expect(subject.mass).to be_an_instance_of BigDecimal
+    		end
+    	end
+    	describe "#density" do
+    		it "should == 1" do
+    			expect(subject.density).to eq 1
+    		end
+    	end
+    	describe "#gravity" do
+    		it "should == 1" do
+    			expect(subject.density).to eq 1
+    		end
+    	end
+    end
+end
+    	
